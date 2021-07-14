12d77209ddff8ed763010d58fb4a7fdc572df545
Elizabeth McDaniel <emcdaniel@wisc.edu>
2021-07-14 17:52:25 -0500

    orthogroup labels for uw1 and uw3 to export for flanking project

--- scripts/parse-orthogroups.R
+++ scripts/parse-orthogroups.R
@@ -1,7 +1,6 @@
 library(tidyverse)
 library(reshape2)
 
-
 # all homologs
 homolog_matrix <- read.delim("results/pangenomics/homolog_matrix.txt", sep="\t")
 
@@ -82,6 +81,28 @@ acc_only_final_table <- as.data.frame(acc_only_groups, stringsAsFactors = FALSE)
 colnames(acc_only_final_table)[1] <- "group"
 acc_only_final_table$label <- "acc_core"
 
+# orthologs in uw1 but not in uw3 
+
+uw1_only_groups <- homolog_table %>% 
+  filter(UW1 == 1 & UW3 == 0) %>%
+  rownames_to_column("group") %>% 
+  pull(group)
+
+uw1_only_table <- as.data.frame(uw1_only_groups)
+uw1_only_table$label <- "accessory"
+colnames(uw1_only_table)[1] <- "group"
+
+# orthologs in uw3 but not in uw1
+
+uw3_only_groups <- homolog_table %>% 
+  filter(UW3 == 1 & UW1 == 0) %>%
+  rownames_to_column("group") %>% 
+  pull(group)
+
+uw3_only_table <- as.data.frame(uw3_only_groups)
+uw3_only_table$label <- "accessory"
+colnames(uw3_only_table)[1] <- "group"
+
 # groups to locus tags
 # Do for UW1, UW3, UW5, and UW7 to show for the beginning enrichment SNVs/diversity of those loci?
 # Gene nucleotide diversity by the locus tag
@@ -96,6 +117,8 @@ core_locus_tags <- left_join(core_table, uw_locus_tags)
 
 acc_locus_tags <- left_join(acc_only_final_table, uw_locus_tags)
 
+uw3_accessory_locus_tags <- left_join(uw3_only_table, uw_locus_tags)
+
 # UW1 SNVs to ortholog definitions
 uw1_core_locus_tags <- core_locus_tags %>% 
   select(group, label, UW1)
@@ -105,10 +128,63 @@ uw1_accum_locus_tags <- acc_locus_tags %>%
   select(group, label, UW1)
 colnames(uw1_accum_locus_tags)[3] <- "locus_tag"
 
+uw1_accessory_locus_tags <- left_join(uw1_only_table, uw_locus_tags) %>% 
+  select(group, label, UW1)
+colnames(uw1_accessory_locus_tags)[3] <- "locus_tag"
+
+# diversity table for uw1 
 uw1_2005_snvs <- read_tsv("results/SNVs/R1R2_vs_R3R4/R1R2_2005_UW1_gene_info.tsv") %>% 
   select(gene, nucl_diversity)
 colnames(uw1_2005_snvs)[1] <- "locus_tag"
 uw1_2005_snvs$locus_tag <- gsub("gnl\\|X\\|", "", uw1_2005_snvs$locus_tag)
 
+# core tags div table 
 uw1_core_tags_snvs <- left_join(uw1_core_locus_tags, uw1_2005_snvs)
+# acc core tags div table
 uw1_accum_tags_snvs <- left_join(uw1_accum_locus_tags, uw1_2005_snvs)
+# uw1 accessory tags div table
+uw1_accessory_tags_snvs <- left_join(uw1_accessory_locus_tags, uw1_2005_snvs)
+# combine all three for a UW1 table 
+
+uw1_groups_cogs_div_table <- rbind(uw1_core_tags_snvs, uw1_accum_tags_snvs, uw1_accessory_tags_snvs) %>% 
+  mutate(reference = "UW1") %>% 
+  select(reference, label, locus_tag)
+
+
+# UW3 SNVS/diversity table to merge with locus tags
+
+uw3_core_locus_tags <- core_locus_tags %>% 
+  select(group, label, UW3)
+colnames(uw3_core_locus_tags)[3] <- "locus_tag"
+
+uw3_accum_locus_tags <- acc_locus_tags %>% 
+  select(group, label, UW3)
+colnames(uw3_accum_locus_tags)[3] <- "locus_tag"
+
+uw3_accessory_locus_tags <- left_join(uw3_only_table, uw_locus_tags) %>% 
+  select(group, label, UW3)
+colnames(uw3_accessory_locus_tags)[3] <- "locus_tag"
+
+# diversity table for uw3
+uw3_2005_snvs <- read_tsv("results/SNVs/R1R2_vs_R3R4/R1R2_2005_UW3_gene_info.tsv") %>% 
+  select(gene, nucl_diversity)
+colnames(uw3_2005_snvs)[1] <- "locus_tag"
+uw3_2005_snvs$locus_tag <- gsub("gnl\\|X\\|", "", uw3_2005_snvs$locus_tag)
+
+# core tags div table 
+uw3_core_tags_snvs <- left_join(uw3_core_locus_tags, uw3_2005_snvs)
+# acc core tags div table
+uw3_accum_tags_snvs <- left_join(uw3_accum_locus_tags, uw3_2005_snvs)
+# uw1 accessory tags div table
+uw3_accessory_tags_snvs <- left_join(uw3_accessory_locus_tags, uw3_2005_snvs)
+# combine all three for a UW1 table 
+
+uw3_groups_cogs_div_table <- rbind(uw3_core_tags_snvs, uw3_accum_tags_snvs, uw3_accessory_tags_snvs) %>% 
+  mutate(reference = "UW3") %>% 
+  select(reference, label, locus_tag)
+
+###### combined group/locustag table for both uw1 and uw3 for use in flanking data 
+# come back later for using Uw1/Uw3 in the AcDiv data
+
+uw_combined_cogs_tags_table <- rbind(uw1_groups_cogs_div_table, uw3_groups_cogs_div_table)
+write.csv(uw_combined_cogs_tags_table, "results/pangenomics/UW1_UW3_COGS_TAGS_TABLE.csv", row.names = FALSE, quote=FALSE)
